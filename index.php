1f7e2ad3736ecbe1be3e119f31478a5bcd5fb90b
Anthony Paterson <arpaterson@live.com>
2015-06-24 12:48:48 +1200

    fixed relative refs after folder restructure, added home page, some new buttons and home button

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+ <head>
+  <link rel="stylesheet" type="text/css" href="/css/main.css">
+  <?php 
+  ?>
+  <title>Opus Research 5-27804.00 </title>
+ </head>
+ 
+ <body>
+  <h1>NZTA</h1>
+  <h2>Instrumented Bicycle</h2>
+  <h3>5-27804.00>
+  <table>
+   <tbody>
+    <tr>
+     <td>
+      <form method="get" action="/">
+       <button type="submit">Home</button>
+      </form>
+     </td>
+    </tr>
+    <tr>
+     <td>
+      <form method="get" action="/control">
+       <button type="submit">Control</button>
+      </form>
+     </td>
+    </tr>
+    <tr>
+     <td>
+      <form method="get" action="/logs">
+       <button type="submit">Logs</button>
+      </form>
+     </td>
+    </tr>
+    <tr>
+     <td>
+      <form method="get" action="/">
+       <button type="submit">DL Logs as ZIP (Not working)</button>
+      </form>
+     </td>
+    </tr>
+   </tbody>
+  </table>
+
+
+ </body>
+
+</html>
